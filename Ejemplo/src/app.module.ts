ef61c1723f6a925ec5339e9a928588cfb946e08b
dennis238 <dennis.238@hotmail.com>
2018-05-09 11:39:13 -0500

    Pull Clase

---
+++ Ejemplo/src/app.module.ts
@@ -0,0 +1,20 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {UsuarioController} from "./usuario.controller";
+import {ParametrosController} from "./parametros.controller";
+
+@Module({
+    imports: [ // OtrosModulos
+
+    ],
+    controllers: [ // Controladores
+        AppController,
+        UsuarioController,
+        ParametrosController
+    ],
+    components: [
+
+    ],
+})
+export class AppModule {
+}
---
+++ Ejemplo/src/parametros.controller.ts
@@ -0,0 +1,22 @@
+import {Body, Controller, Param, Post, Query, Req, Res} from "@nestjs/common";
+
+@Controller('Parametros')
+export class ParametrosController {
+
+    @Post('devolver/:id/:modelo')
+    devolverParametros(
+        @Req() request,
+        @Res() response,
+        @Query() queryParams,
+        @Body() bodyParams,
+        @Param() paramsParams
+    ) {
+        const respuesta = {
+            queryParams: queryParams,
+            bodyParams: bodyParams,
+            paramsParams: paramsParams
+        };
+        return response.send(respuesta);
+    }
+
+}
